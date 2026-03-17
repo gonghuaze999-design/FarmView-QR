884c7aff109d95b763a1e5f8601e51f4a7ad7bc2
gonghuaze999-design <gonghuaze999@gmail.com>
2026-03-17 11:39:27 +0800

    Create SiteContext.tsx

---
+++ src/contexts/SiteContext.tsx
@@ -0,0 +1,33 @@
+import React, { createContext, useContext } from 'react';
+
+export type SiteBinding = {
+  siteName: string;
+  weatherId: number;
+  insectId: number;
+  cameraId: number;
+  farmlandId?: number; // 大数据平台的地块ID
+};
+
+type SiteContextType = {
+  siteKey: string;
+  binding: SiteBinding | null;
+};
+
+const SiteContext = createContext<SiteContextType>({
+  siteKey: 'base-current',
+  binding: null,
+});
+
+export const useSiteContext = () => useContext(SiteContext);
+
+export const SiteProvider: React.FC<{
+  siteKey: string;
+  binding: SiteBinding | null;
+  children: React.ReactNode;
+}> = ({ siteKey, binding, children }) => {
+  return (
+    <SiteContext.Provider value={{ siteKey, binding }}>
+      {children}
+    </SiteContext.Provider>
+  );
+};
